3d6d34c2b81e1bbaddad5ec37f4167afcaadb894
Jeremy Marshall Abbott <jeremymabbott@gmail.com>
2016-01-03 13:41:11 -0600

    Adds css and logo

--- src/SuaveMusicStore/App.fs
+++ src/SuaveMusicStore/App.fs
@@ -25,6 +25,7 @@ let main argv =
             path Path.Store.overview >>= OK "Store"
             path Path.Store.browse >>= browse
             pathScan Path.Store.details (fun id -> OK (sprintf "Details: %d" id))
+            pathRegex "(.*)\.(css|png)" >>= Files.browseHome
         ]
 
     startWebServer defaultConfig webPart
--- src/SuaveMusicStore/View.fs
+++ src/SuaveMusicStore/View.fs
@@ -6,10 +6,13 @@ let divId id = divAttr ["id", id]
 let h1 xml = tag "h1" [] xml
 let aHref href = tag "a" ["href", href]
 
+let cssLink href = linkAttr ["href", href; "rel", "stylesheet"; "type", "text/css"]
+
 let index =
     html [
         head [
             title "Suave Music Store"
+            cssLink "/Site.css"
         ]
 
         body [
@@ -18,7 +21,7 @@ let index =
             ]
 
             divId "footer" [
-                text "built with "
+                text "Built with "
                 aHref "http://fsharp.org" (text "F#")
                 text " and "
                 aHref "http://suave.io" (text "Suave.IO")
